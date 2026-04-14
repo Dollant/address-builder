a22caf316e46fb277e78273aa99f77e016d9ba57
Dollant <ariawilliams2425@gmail.com>
2026-04-14 13:41:12 -0400

    Added readRequired() helper for non‑empty input. Created a reusable method that forces the user to enter a non‑blank value. Preventing empty fields and trimming whitespace.

--- src/main/java/com/pluralsight/AddressBuilderApp.java
+++ src/main/java/com/pluralsight/AddressBuilderApp.java
@@ -1,4 +1,28 @@
 package com.pluralsight;
 
+import java.util.Scanner;
+
 public class AddressBuilderApp {
+    public static void main(String[] args) {
+        Scanner keyboard = new Scanner(System.in);
+        StringBuilder sb = new StringBuilder();
+
+        System.out.println("Please provide the following info:");
+
+        String fullName = readRequired(keyboard, "Full name: ");
+        sb.append(fullName).append("\n\n");
+
+
+    }
+
+    private static String readRequired(Scanner keyboard, String prompt) {
+        while (true) {
+            System.out.print(prompt);
+            String input = keyboard.nextLine().trim();
+            if(!input.isEmpty()) {
+                return input;
+            }
+        }
+
+    }
 }
